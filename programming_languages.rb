a677bf073e64961c12da43bf15af77d65638e5a5
Linda Chang <lindachang186@gmail.com>
2018-10-24 21:47:48 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,16 +2,16 @@ require "pry"
 
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |key, value|
-    value.each do |language, data|
+  languages.each do |k, v|
+    v.each do |language, data|
       data.each do |type, info|
         if new_hash[language] == nil
           new_hash[language]= { type=> info, :key => [key]}
         else
           new_hash[language][:key] << key
         end
+      end
+    end
   end
-end
-end
-return new_hash
+    return new_hash
 end
