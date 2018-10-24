753379fec51b3770068584deb1bdba2fad91187a
Linda Chang <lindachang186@gmail.com>
2018-10-24 20:42:39 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |key, value|
     value.each do |language, data|
-      data.each do |type, value|
+      data.each do |type, info|
       binding.pry
       hash[language] = {}
     end
